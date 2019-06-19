ffc9f06041687196e5733e129106ce9aa93bd197
Peter Donker <peter@bring2mind.net>
2019-06-19 12:56:58 +0200

    Remove simple tasks for a more targeted one

---
+++ Commands/Environment/InstallDnn.cs
@@ -0,0 +1,34 @@
+﻿using Dnn.Powershell.Local.Environment;
+using System.IO;
+using System.Management.Automation;
+
+namespace Dnn.Powershell.Local.Commands.Environment
+{
+    [Cmdlet(VerbsLifecycle.Install, Nouns.Dnn)]
+    public class InstallDnn : DNNCmdLet
+    {
+        [Parameter(Position = 0, Mandatory = true)]
+        public string ZipFolder { get; set; }
+
+        [Parameter(Position = 1, Mandatory = true)]
+        public string Version { get; set; }
+
+        [Parameter(Position = 2, Mandatory = true)]
+        public string TargetFolder { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public string Type { get; set; } = "Install";
+
+        protected override void ProcessRecord()
+        {
+
+            var zip = CompressionController.FindDnnZip(ZipFolder, Version, Type);
+            if (string.IsNullOrEmpty(zip))
+            {
+                throw new System.Exception(string.Format("Cannot find DNN install zip for version {0} in folder {1}", Version, ZipFolder));
+            }
+            DiskController.CreateIisFolder(TargetFolder);
+            CompressionController.UnzipFile(Path.Combine(ZipFolder, zip), TargetFolder);
+        }
+    }
+}
